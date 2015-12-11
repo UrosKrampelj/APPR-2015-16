5aeaf1330f952be20364efae93789b18390bd269
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2015-12-11 11:37:29 +0100

    Uvoz tabele s spletne strani.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,14 @@
-# 2. faza: Uvoz podatkov
+library(rvest)
+library(dplyr)
+library(gsubfn)
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
-}
+html <- html_session("https://en.m.wikipedia.org/wiki/List_of_Chelsea_F.C._players#List_of_players") %>% read_html()
+html_tabela <- html %>% html_nodes(xpath="//table[1]") %>% .[[1]]
+tabela <- html_tabela %>% html_table()
+tabela[grep(",", tabela[[1]]), 1] <- html_tabela %>%
+  html_nodes(xpath="//span[@class='vcard']//a[@title]") %>%
+  html_text()
+Encoding(tabela[[1]]) <- "UTF-8"
+Encoding(tabela[[2]]) <- "UTF-8"
+Encoding(tabela[[4]]) <- "UTF-8"
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
-
-obcine <- uvozi.obcine()
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
