e6274c2c55e5d14142721cc23190b02c59c03620
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2015-12-14 20:05:05 +0100

    Uvoz tabele 2 in brisanje nekaterih stolpcev in vrstic.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -12,3 +12,30 @@ Encoding(tabela[[1]]) <- "UTF-8"
 Encoding(tabela[[2]]) <- "UTF-8"
 Encoding(tabela[[4]]) <- "UTF-8"
 
+
+html2 <- html_session("https://en.wikipedia.org/wiki/List_of_Chelsea_F.C._seasons") %>% read_html()
+html_tabela2 <- html2 %>% html_nodes(xpath="//table[3]") %>% .[[1]]
+tabela2 <- html_tabela2 %>% html_table(fill= TRUE)
+
+Encoding(tabela2[[2]]) <- "UTF-8"
+Encoding(tabela2[[3]]) <- "UTF-8"
+Encoding(tabela2[[1]]) <- "UTF-8"
+
+ tabela2[[1, 1]] <- ("SEZONA")
+ tabela2[[1, 2]] <- ("LIGA")
+
+ tabela2[[1, 3]] <- ("TEKME")
+ tabela2[[1, 4]] <- ("ZMAGE")
+ tabela2[[1, 5]] <- ("NEODLOČENO")
+ tabela2[[1, 6]] <- ("PORAZ")
+ tabela2[[1, 7]] <- ("DANI GOLI")
+ tabela2[[1, 8]] <- ("PREJETI GOLI")
+ tabela2[[1, 9]] <- ("TOČKE")
+ tabela2[[1, 11]] <- ("POKAL FA")
+ tabela2[[1, 10]] <- ("UVRSTITEV")
+ tabela2[[1, 12]] <- ("LIGAŠKI POKAL")
+ tabela2[[1, 14]] <- ("LIGA PRVAKOV")
+ tabela2[[1, 20]] <- ("NAJBOLJŠI STRELEC")
+ tabela2[[1, 21]] <- ("DOSEŽENI GOLI")
+
+ newtabela2 <- tabela2[c(-12, -33, -34, -104, -105, -106), c(-13, -15, -16, -17, -18, -19)]
