ca1c414962800b021b908392b3c345625edb04c3
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2015-12-18 12:13:06 +0100

    Uvoz tebela2 in čiščenje tabele.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -12,3 +12,16 @@ Encoding(tabela[[1]]) <- "UTF-8"
 Encoding(tabela[[2]]) <- "UTF-8"
 Encoding(tabela[[4]]) <- "UTF-8"
 
+
+html2 <- html_session("https://en.wikipedia.org/wiki/List_of_Chelsea_F.C._seasons") %>% read_html(encoding="UTF-8")
+html_tabela2 <- html2 %>% html_node(xpath="//table[@class='wikitable plainrowheaders']")
+tabela2 <- html_tabela2 %>% html_table(fill = TRUE)
+Encoding(tabela2[[1]]) <- "UTF-8"
+Encoding(tabela2[[2]]) <- "UTF-8"
+
+
+colnames(tabela2 ) -> imena
+tabela2 <- tabela2[-c(12, 33, 104,  105, 106), -c( 12, 13,14,  15, 16, 17, 18,19)]
+imena <- c("Sezona", "Liga", "Št. tekem", "Zmage", "izenačeno", "Porazi", "št. zadetih golov", "Št danih golov", "Točke", "Položaj", "FA pokal", "Najboljši strelec", "Št. golov")
+
+
