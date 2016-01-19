22f35bc94d4fd7a788c0063d155da4f678839a61
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2016-01-19 16:51:10 +0100

    Začetek uvoza zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,17 @@
 # 3. faza: Izdelava zemljevida
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, zemljevid, "OB_UIME", c("Ankaran", "Mirna"))
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+# 3. faza: Izdelava zemljevida
+# Uvozimo funkcijo za pobiranje in uvoz zemljevida.
+source("lib/uvozi.zemljevid.r")
+# Uvozimo zemljevid.
+cat("Uvazam zemljevid sveta...\n")
+svet <- uvozi.zemljevid("http://www.naturalearthdata.com/ || http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_map_units.zip",
+                        "svet", "ne_110m_admin_0_map_units.shp", mapa = "zemljevid",
+                        encoding = "Windows-1252")
+svet1<- svet[svet$continent %in% c("Europe", "Africa","South America","Asia","North America"),]
+
+drzave <- table(IGRALCI$Nationality)
+
