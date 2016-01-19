069dd937b3fd05663891ee4f07a3e098710e40c1
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2016-01-19 17:05:20 +0100

    Uvoz zemljevida posodobljen

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -9,9 +9,10 @@ source("lib/uvozi.zemljevid.r")
 # Uvozimo zemljevid.
 cat("Uvazam zemljevid sveta...\n")
 svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_map_units.zip",
-                        "svet", "ne_110m_admin_0_map_units.shp", mapa = "zemljevid",
+                        "svet", "ne_110m_admin_0_map_units.shp", mapa = "Zemljevid",
                         encoding = "Windows-1252")
 svet1<- svet[svet$continent %in% c("Europe", "Africa","South America","Asia","North America"),]
 
 drzave <- table(IGRALCI$Nationality)
+
 
