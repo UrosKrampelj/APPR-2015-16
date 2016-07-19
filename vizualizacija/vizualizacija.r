199522fe50abc1cec653f8148c37c3693b2b5ebb
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2016-07-19 13:34:30 +0200

    Uvoz zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -5,13 +5,21 @@
 
 # 3. faza: Izdelava zemljevida
 # Uvozimo funkcijo za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r")
+source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
+library(ggplot2)
+library(dplyr)
+
 # Uvozimo zemljevid.
 cat("Uvazam zemljevid sveta...\n")
-svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_map_units.zip",
-                        "svet", "ne_110m_admin_0_map_units.shp", mapa = "Zemljevid",
-                        encoding = "Windows-1252")
+
+
+svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip",
+                         "ne_110m_admin_0_countries", encoding = "Windows-1252")
+
 svet1<- svet[svet$continent %in% c("Europe", "Africa","South America","Asia","North America"),]
+
+
+
 
 drzave <- table(IGRALCI$Nationality)
 imenadrzav<-names(drzave)
@@ -22,8 +30,8 @@ names(barve) <- names(drzave)
 barve.zemljevid <- barve[as.character(svet1$name_long)]
 barve.zemljevid[is.na(barve.zemljevid)] <- "white"
 
-mojsvet <- svet1[svet1$name_long %in% imenadrzav,]
-koordinate <- coordinates(mojsvet)
-imena.drzav <- as.character(mojsvet$name_long)
+#mojsvet <- svet1[svet1$name_long %in% imenadrzav,]
+#koordinate <- coordinates(mojsvet)
+#imena.drzav <- as.character(mojsvet$name_long)
 
 
