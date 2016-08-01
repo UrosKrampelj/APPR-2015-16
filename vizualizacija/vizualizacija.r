e36c0d96c35d286cd5f24185801fc6c228734eca
Uroš Krampelj <uros.krampelj@student.fmf.uni-lj.si>
2016-08-01 11:45:22 +0200

    Razširitev zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -16,7 +16,7 @@ cat("Uvazam zemljevid sveta...\n")
 svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_map_units.zip",
                          "ne_110m_admin_0_map_units", encoding = "Windows-1252")
 
-svet1<- svet[svet$continent %in% c("Europe", "Africa","South America","Asia","North America"),]
+svet1<- svet[svet$continent %in% c("Europe", "Africa","South America","Asia","North America", "Oceania"),]
 
 
 
@@ -38,5 +38,8 @@ mojsvet <- svet1[svet1$name_long %in% imenadrzav, ]
 koordinate <- coordinates(mojsvet)
 imena.drzav <- as.character(mojsvet$name_long)
 rownames(koordinate) <- imena.drzav
+
+plot(svet1, xlim=c(-180, 180), ylim=c(-33,73), col=barve.zemljevid, bg="lightblue")
+
 
 
